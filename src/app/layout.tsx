a67e9b3bf6d9725f8d7701b0d935a56abbd9132d
Lin Li <lli128@outlook.com>
2025-11-05 09:51:55 -0500

    Fix formatting of title in metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "CanLearn:Dr. Li's Learning Clinic",
+  title: "CanLearn: Dr. Li's Learning Clinic",
   description: "Dr. Li's Learning Clinic",
 };
 
